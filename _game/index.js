7f36004aeb1bb85b991f5936e255642faa5494b9
Archianne <helena19w@gmail.com>
2021-10-03 19:29:26 +0100

    .

--- _game/index.js
+++ _game/index.js
@@ -1,6 +1,23 @@
-const username = window.localStorage.getItem('username');
+const username = window.localStorage.getItem("username");
 
-console.log(username)
+const option = document.querySelectorAll(".option");
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const lizard = document.querySelector(".lizard");
+const spock = document.querySelector(".spock");
+const playerChoice = document.querySelector(".player-choice");
+const computerChoice = document.querySelector(".computer-choice");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+const playerOptions = document.querySelector(".player-options");
+
+console.log(username);
+
+const getValue = (e) => {
+    console.log(this.getAttribute("data-value"));
+};
 
 // Scissors cuts Paper
 // Paper covers Rock
